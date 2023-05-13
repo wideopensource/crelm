8fb2c1c5a7a9acc568936ef92cc7f682e06466a2
wideopentech <fossie@wideoppentech.co.uk>
2023-05-13 19:14:50 +0100

    remove Paste.string (use str instead)

--- crelm/libcrelm.py
+++ crelm/libcrelm.py
@@ -8,4 +8,4 @@ class LibCrelm:
 
         self._lib.make_header(self._source_buffer, self._header_buffer)
 
-        return self._lib.string(self._header_buffer).strip()
+        return self._lib.str(self._header_buffer).strip()
